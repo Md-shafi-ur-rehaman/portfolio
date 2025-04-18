e0d9022a7b38840d2b1452bb8e2a7dd4daef6da8
md-shafi-ur-rehaman <mohammadshafiq.rahaman@gmail.com>
2025-04-18 23:58:30 +0530

    created every file

---
+++ components/Hero.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MagicButton from './MagicButton'
+import { FaLocationArrow } from "react-icons/fa6";
+
+const Hero = () => {
+  return (
+    <div className="">
+      
+    </div>
+  )
+}
+
+export default Hero
---
+++ components/RecentProjects.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import { FaLocationArrow } from "react-icons/fa6";
+import { FaReact } from "react-icons/fa";
+
+import { projects } from "@/data";
+
+const RecentProjects = () => {
+  return (
+    <div className="">
+      Projects
+    </div>
+  );
+};
+
+export default RecentProjects;
